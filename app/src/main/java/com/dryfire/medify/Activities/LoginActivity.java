c0b0c757e0a40b5444370e4b9bcf6e43807814c4
Saurabh Tripathi <kstripathi7!gmail.com>
2020-12-28 18:41:46 +0530

    Added Login Page

--- app/src/main/java/com/dryfire/medify/Activities/LoginActivity.java
+++ app/src/main/java/com/dryfire/medify/Activities/LoginActivity.java
@@ -1,17 +1,40 @@
 package com.dryfire.medify.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dryfire.medify.R;
+import com.dryfire.medify.Util.SharedPrefs;
+import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
 
+    SharedPrefs sharedPrefs;
+    private MaterialButton loginButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        sharedPrefs = new SharedPrefs(this);
+        if(sharedPrefs.loadNightModeState() == true){
+            setTheme(R.style.darktheme);
+        }else{
+            setTheme(R.style.AppTheme);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (MaterialButton) findViewById(R.id.signin_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                finish();
+            }
+        });
     }
 }
